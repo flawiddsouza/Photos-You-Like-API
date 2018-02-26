a37dd9aa28f0b8600102f2d224df1731d11ba490
Flawid DSouza <flawid@live.in>
2018-02-26 21:32:41 +0530

    Fixed thumbnail generation script for previously added images

--- scripts/generate thumbnails for images added before thumbnail generation was added.js
+++ scripts/generate thumbnails for images added before thumbnail generation was added.js
@@ -10,12 +10,13 @@ const fs = require('fs')
 
 fs.readdirSync(uploadsDir).forEach(file => {
     var fileExtension = path.extname(file)
-    if(fileExtension === '.jpg' || fileExtension === '.png' || fileExtension === '.png_large') { // .png_large is for images added from twitter scraping
+    // .png:large, .jpg:large are for images added from twitter scraping
+    // in windows, they are stored as .png_large & .jpg_large
+    if(fileExtension === '.jpg' || fileExtension === '.png' || fileExtension === '.png:large' || fileExtension === '.jpg:large'|| fileExtension === '.png_large' || fileExtension === '.jpg_large') {
         let imagePath = path.join(uploadsDir, file)
-        console.log(imagePath)
         let thumbnailSavePath = path.join(thumbnailsDir, file)
-        sharp(imagePath).resize(thumbnailWidth).jpeg({ quality: 95 }).toFile(thumbnailSavePath).then(data => {
+        sharp(fs.readFileSync(imagePath)).resize(thumbnailWidth).jpeg({ quality: 95 }).toFile(thumbnailSavePath).then(data => {
             console.log(thumbnailSavePath)
-        })
+        }).catch(e => console.log(file, e))
     }
 })
